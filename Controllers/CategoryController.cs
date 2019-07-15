cd800a89c54add60768488bec36f7743188efe55
ZeroAT <zeroat@live.com>
2019-07-15 14:04:05 -0400

    Create GET/POST/View

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using ProductManagement.Data;
 using ProductManagement.Models;
 
@@ -13,16 +14,43 @@ namespace ProductManagement.Controllers
     {
 
         private readonly ProductContext _context;
-        // GET: Category/list
 
         public CategoryController(ProductContext context)
         {
             _context = context;
         }
+
         public async Task<IActionResult> List()
         {
 
             return View(await _context.Categories.ToListAsync());
+        }
+
+        //GET:
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name")] Category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(category);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(List));
+                }
+            }
+            catch(DbUpdateException)
+            {
+                ModelState.AddModelError("", "unable to save saves");
+            }
+            return View(category);
         }
     }
 }
